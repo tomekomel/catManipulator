3ba9d2fee70557605296830f3844115c6cfd0893
Tomasz Omeljaniuk <tomekomel@gmail.com>
2019-03-10 15:56:28 +0100

    Image reading by Promises

--- manipulator.js
+++ manipulator.js
@@ -1,31 +1,16 @@
-const arithmetic = require('arithmetic');
 const axios = require('axios');
 
-const res = arithmetic.add(37, 37);
-
-//axios.default.headers.common['x-api-key'] = 'e3253df8-8a0b-4043-9969-a19c7f8f2651';
-
 const imagePromise = axios.get('https://api.thecatapi.com/v1/images/search');
-imagePromise.then(
-  setCatImage,
-  logError
-);
 
-function setCatImage(response) {
-  document.getElementById('cat_image').setAttribute('src', response.data[0].url);
+function loadImageAsync(url) {
+ return new Promise((resolve, reject) => {
+   const img = new Image();
+   img.addEventListener('load', event => resolve(img));
+   img.addEventListener('error', reason => reject(new Error('Loading faild')));
+   img.src = url;
+ });
 }
 
-function logError(reason)
-{
-  console.log('Error log: ' + reason);
-}
-
-const newPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(new Error("itpracownia"));
-  }, 2000);
-});
-
-newPromise
-  .then(result => console.log(result))
-  .catch(reject => console.log(reject));
+loadImageAsync('http://thecatapi.com/api/images/get?format=src&type=jpg&size=medium')
+  .then(img => document.querySelector('.image').appendChild(img))
+  .catch(reason => console.log(reason));
