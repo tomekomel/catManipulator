02782dfcdb3fe9a64f4f8d71be2f87649a2cb292
Tomasz Omeljaniuk <tomekomel@gmail.com>
2019-03-10 19:16:37 +0100

    Movie fetching by Promise chaining

--- manipulator.js
+++ manipulator.js
@@ -5,6 +5,38 @@ const p = new Promise((resolve, reject) => {
 });
 
 p.then(result => result * 2)
-  .then(result => Promise.reject(Error(`Ops!`)))
+  .then(result => result * 2)
   .then(result => console.log(result))
   .catch(reason => console.log(reason));
+
+const movies = [{id: 1, categoryId: 1, title: 'The Help'}];
+
+const categories = [{id: 1, name: 'Fabulary'}];
+
+function fetchMovie(movieId) {
+  return new Promise((resolve, reject) => {
+    const movie = movies.find(movie => movie.id === movieId);
+    if (movie) {
+      resolve(movie);
+    }
+    reject(Error(`Movie was not found`));
+  });
+}
+
+function populateCategory(movie) {
+  return new Promise((resolve, reject) =>
+  {
+    const category = categories.find(category => category.id === movie.categoryId);
+    if (category) {
+      movie.category = category.name;
+      resolve(movie);
+    }
+    reject(Error(`Category was not foudn`));
+  });
+}
+
+fetchMovie(1)
+  .then(movie => populateCategory(movie))
+  .then(result => console.log(result))
+  .catch(reason => console.log(reason));
+
